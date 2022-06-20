588383b9ffd32752052c729e6a37a551e58dc7ec
Magdalena Joseph <magdalena.joseph@outlook.com>
2022-06-20 13:30:06 -0400

    created cats array

---
+++ script.js
@@ -0,0 +1,18 @@
+
+
+  //cats array
+ let catArray = [
+   { img: "https://i.imgur.com/7yWazQM.jpg", id: 1, name: "fluffy" },
+   { img: "https://i.imgur.com/Ui4Ea4d.jpg", id: 2, name: "bella" },
+   { img: "https://i.imgur.com/5N3I2xt.jpg", id: 3, name: "daisy" },
+   { img: "https://i.imgur.com/Z39MxEg.jpg", id: 4, name: "oli" },
+   { img: "https://i.imgur.com/aK0Ki8S.jpg", id: 5, name: "lola" },
+   { img: "https://i.imgur.com/Kx8mqhe.jpg", id: 6, name: "lily" },
+   { img: "https://i.imgur.com/7yWazQM.jpg", id: 1, name: "fluffy" },
+   { img: "https://i.imgur.com/Ui4Ea4d.jpg", id: 2, name: "bella" },
+   { img: "https://i.imgur.com/5N3I2xt.jpg", id: 3, name: "daisy" },
+   { img: "https://i.imgur.com/Z39MxEg.jpg", id: 4, name: "oli" },
+   { img: "https://i.imgur.com/aK0Ki8S.jpg", id: 5, name: "lola" },
+   { img: "https://i.imgur.com/Kx8mqhe.jpg", id: 6, name: "lily" },
+  
+ ];
